53f9070bc7f878661986f7b0770e7a1d0a11cd00
“HIT_陈帆” <2513151911@qq.com>
2023-11-04 18:14:27 +0800

    update: PageIndex新增目录

--- src/pages/ArticlePage/ArticleContent.js
+++ src/pages/ArticlePage/ArticleContent.js
@@ -1,53 +1,86 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-function getArticleFileName(catalogue,page,articleKey){
-  if(!catalogue[page]) return 'index.md';
-  let result='index.md';
-  Object.keys(catalogue[page].folders).forEach((folderKey)=>{
-    Object.keys(catalogue[page].folders[folderKey].articles).forEach((article)=>{
-      if(article===articleKey){
-        result=catalogue[page].folders[folderKey].articles[article].fileName;
+function getArticleFileName(catalogue, page, articleKey) {
+  if (!catalogue[page]) return 'index.md';
+  let result = 'index.md';
+  Object.keys(catalogue[page].folders).forEach((folderKey) => {
+    Object.keys(catalogue[page].folders[folderKey].articles).forEach((article) => {
+      if (article === articleKey) {
+        result = catalogue[page].folders[folderKey].articles[article].fileName;
         return;
       }
     });
   });
- return result;
+  return result;
 }
 export default function ArticleContent(props) {
   const {
     page,
     article,
-  }=useParams();
+  } = useParams();
   const {
     catalogue,
-  }=useSelector(state=>{
-    return{
-      catalogue:state.catalogueStage,
+  } = useSelector(state => {
+    return {
+      catalogue: state.catalogueStage,
     };
   });
-  console.log('page1',page);
-  console.log('article1',article);
-  const article_fileName=getArticleFileName(catalogue,page,article);
+  const article_fileName = getArticleFileName(catalogue, page, article);
+  if (article === 'index') {
+    return catalogue[page] && (
+      <div style={{ margin: '2rem',padding:'0rem',flex:'1' }}>
+        <h2>{catalogue[page].title}</h2>
+        
+        {
+          Object.keys(catalogue[page].folders).map((folderKey) => (
+            <div key={folderKey} style={{marginTop:'1.6rem'}}>
+              <h4 style={{}}>{catalogue[page].folders[folderKey].title}</h4>
+              {
+                Object.keys(catalogue[page].folders[folderKey].articles).map((articleKey) => (
+                  <>
+                    <NavLink
+                      key={articleKey}
+                      to={`/article/${page}/${articleKey}`}
+                      className='article-link'
+                      style={{
+                        color:'var(--secondary-color-dark)',
+                        textDecoration:'underline',
+                      }}
+                    >
+                      {catalogue[page].folders[folderKey].articles[articleKey].title}
+                    </NavLink>
+                    &nbsp; | &nbsp;
+                  </>
+                ))
+              }
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
   return (
     <div
-    style={{
-      flex:1,
-      backgroundColor:'#fff',
-      overflow:'hidden'
-    }}
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        overflow: 'hidden'
+      }}
     >
-      <iframe src={
-            article_fileName.match(/\.html$/)?
-            `/data/article/${page}/${article_fileName}`:
-            article_fileName.match(/\.md$/)?
-            `/data/article/${page}/md-reader.html?mdurl=${article_fileName}`
-            :null
-      }
-            width="100%"
-            height="100%"
-            frameborder="0"
-          ></iframe>
+      <iframe
+        src={
+          article_fileName.match(/\.html$/) ?
+            `/data/article/${page}/${article_fileName}` :
+            article_fileName.match(/\.md$/) ?
+              `/data/article/${page}/md-reader.html?mdurl=${article_fileName}`
+              : null
+        }
+        width="100%"
+        height="100%"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
